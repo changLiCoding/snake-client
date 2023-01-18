cce2378ce8c1488870f9ba7f8127f6c01ac849b4
javascriptsucks <evix1988@live.cn>
2023-01-18 15:44:38 -0600

    keyboard-input-capture

--- play.js
+++ play.js
@@ -3,5 +3,25 @@ const connect = require('./client');
 // establishes a connection with the game server
 
 
+// setup interface to handle user input from stdin
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+setupInput();
 console.log("Connecting ...");
 connect();
